2b6d6d25fda74a416bc2d161bcd175bf3dfe9e2e
Raul de Souza <raulsouza180802@gmail.com>
2022-11-22 15:46:21 -0300

    feat: vendo se vai fucionar ne cara, pq ta complicado essa situação do brasil

--- src/app.tsx
+++ src/app.tsx
@@ -7,6 +7,9 @@ export function App() {
     <div className={contraste ? "bodyBlack" : "bodyWhite"}>
       <h1>FALA DEVII</h1>
       <h2>Raul</h2>
+      <h5>
+        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+      </h5>
       <img
         src="../mao.png"
         className={contraste ? "gatoDaAliciaComFomeLigado" : ""}
